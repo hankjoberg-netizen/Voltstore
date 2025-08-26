351aae8b2fd1745e894e15c46e4ed41bdc446186
hankjoberg-netizen <hankjoberg@gmail.com>
2025-08-26 02:46:53 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 // api/index.js
 const serverless = require('serverless-http');
-const app = require('../server');  // uses your server.js
+const app = require('../server'); // your Express app (from server.js)
 module.exports = serverless(app);
