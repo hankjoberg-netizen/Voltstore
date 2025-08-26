78615bfbeadcd5da52be22d38951d2b9d9eb8c5b
hankjoberg-netizen <hankjoberg@gmail.com>
2025-08-26 01:45:04 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-// Minimal handler to confirm Vercel is serving our function
 module.exports = (req, res) => {
-  if (req.url === '/ping') return res.status(200).send('pong');
-  return res.status(200).send('root ok');
+  res.setHeader('content-type', 'text/plain; charset=utf-8');
+  if (req.url === '/ping') return res.status(200).end('pong');
+  return res.status(200).end('root ok');
 };
