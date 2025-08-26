07869657172eda01a8e2f97deed1a9136cf31b7b
hankjoberg-netizen <hankjoberg@gmail.com>
2025-08-26 02:35:02 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-const serverless = require('serverless-http');
-const app = require('../server');
-module.exports = serverless(app);
+module.exports = (req, res) => {
+  res.status(200).send('home');
+};
